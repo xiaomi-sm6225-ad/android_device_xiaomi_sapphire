5252b84e18f7afb6f7db288974666164f34b9f53
Cosmin Tanislav <demonsingur@gmail.com>
2024-03-22 21:32:27 +0700

    sapphire: Add support for udfps long press sensor
    Change-Id: I7196dce967c9adbc62bb5156ade749c92c7780a1

--- device.mk
+++ device.mk
@@ -30,6 +30,13 @@ PRODUCT_PACKAGES += \
     SystemUIResSapphire \
     WifiResSapphire
 
+# Sensors
+PRODUCT_PACKAGES += \
+    sensors.xiaomi
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
